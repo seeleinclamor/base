2fc8c833f7c96052fd32e0a8d5cc85f168be5d3e
seeleinclamor <38571225+seeleinclamor@users.noreply.github.com>
2022-03-25 17:24:09 -0300

    Update test_root.py

--- tests/test_root.py
+++ tests/test_root.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"Hola": "Mundo"}
